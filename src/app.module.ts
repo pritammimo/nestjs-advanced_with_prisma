aef6c70de840210e61e8a88353b4d64c0270b51f
pritamcn <pritams@capitalnumbers.com>
2022-09-18 20:25:30 +0530

    auth added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { HomeModule } from './home/home.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, PrismaModule, HomeModule],
   controllers: [AppController],
   providers: [AppService],
 })
